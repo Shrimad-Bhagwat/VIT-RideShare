df033eb330bebd23eb8021c9b2ea2f01d81d41c3
Shrimad Bhagwat <51125208+Shrimad-Bhagwat@users.noreply.github.com>
2023-04-15 00:12:36 +0530

    Revert "contact"
    This reverts commit 94c06576570f04e39a9916b918115bceecf27879.

--- app/src/main/java/com/shrimadbhagwat/vitrideshare/DetailActivity.java
+++ app/src/main/java/com/shrimadbhagwat/vitrideshare/DetailActivity.java
@@ -2,19 +2,12 @@ package com.shrimadbhagwat.vitrideshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView from_tv, to_tv, name_tv, date_tv;
-    String contact;
-    Button contact_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +17,6 @@ public class DetailActivity extends AppCompatActivity {
         to_tv = findViewById(R.id.to_tv);
         name_tv = findViewById(R.id.name_tv);
         date_tv = findViewById(R.id.date_tv);
-        contact_btn = findViewById(R.id.contact_button);
-
 
         Bundle bundle = getIntent().getExtras();
 
@@ -34,18 +25,7 @@ public class DetailActivity extends AppCompatActivity {
             to_tv.setText(bundle.getString("To"));
             name_tv.setText(bundle.getString("Name"));
             date_tv.setText(bundle.getString("Date"));
-            contact=(bundle.getString("Contact"));
         }
-        contact_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                Uri x = Uri.parse("tel:(+91)"+contact);
-//                Log.d(Uri.parse(String.valueOf(x)));
-                callIntent.setData(Uri.parse(String.valueOf(x)));
-                startActivity(callIntent);
-            }
-        });
 
     }
 }
